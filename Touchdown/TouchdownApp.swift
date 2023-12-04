6daadd51343d3dc8704c5db2fd93a32057ec94e0
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-12-04 18:18:21 +0200

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Quest76 on 04.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
